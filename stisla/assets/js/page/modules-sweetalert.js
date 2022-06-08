12afc26cb3c78a560c67829ed7a77c7938a412f4
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2022-06-08 22:17:09 +0700

    tambahan

---
+++ stisla/assets/js/page/modules-sweetalert.js
@@ -0,0 +1,61 @@
+"use strict";
+
+$("#swal-1").click(function() {
+	swal('Hello');
+});
+
+$("#swal-2").click(function() {
+	swal('Good Job', 'You clicked the button!', 'success');
+});
+
+$("#swal-3").click(function() {
+	swal('Good Job', 'You clicked the button!', 'warning');
+});
+
+$("#swal-4").click(function() {
+	swal('Good Job', 'You clicked the button!', 'info');
+});
+
+$("#swal-5").click(function() {
+	swal('Good Job', 'You clicked the button!', 'error');
+});
+
+$('.btn-del').on('click',function(e) {
+  e.preventDefault();
+  const href = $(this).attr('href')
+  Swal.fire({
+    title: 'Hapus Data?',
+    text: "Kamu yakin ingin menghapus data?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Hapus!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.location.href = href;
+    }
+  })
+});
+
+$("#swal-7").click(function() {
+  swal({
+    title: 'What is your name?',
+    content: {
+    element: 'input',
+    attributes: {
+      placeholder: 'Type your name',
+      type: 'text',
+    },
+    },
+  }).then((data) => {
+    swal('Hello, ' + data + '!');
+  });
+});
+
+$("#swal-8").click(function() {
+  swal('This modal will disappear soon!', {
+    buttons: false,
+    timer: 3000,
+  });
+});
